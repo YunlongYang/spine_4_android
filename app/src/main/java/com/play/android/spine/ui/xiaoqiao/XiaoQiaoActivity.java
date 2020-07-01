fd9270f1d444a6767909260010d015eecb582eef
yunlong.yang <yunlong.yang@inveno.com>
2020-07-01 18:18:48 +0800

    引用官方画图工具，画的东西多了一些，但是还是不知道画的什么

--- app/src/main/java/com/play/android/spine/ui/xiaoqiao/XiaoQiaoActivity.java
+++ app/src/main/java/com/play/android/spine/ui/xiaoqiao/XiaoQiaoActivity.java
@@ -6,12 +6,13 @@ import androidx.annotation.Nullable;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.esotericsoftware.spine.AndroidSkeletonViewer;
 
 public class XiaoQiaoActivity extends AndroidApplication {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-        initialize(new XiaoQiaoElement(), config);
+        initialize(new AndroidSkeletonViewer(), config);
     }
 }
